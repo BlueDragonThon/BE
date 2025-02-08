54b2401b39916efcf854de95a6572cf2fea1f533
cau20232907 <hs20232907@cau.ac.kr>
2025-02-08 19:32:30 +0900

    College Search By distance

---
+++ src/main/java/com/example/template/domain/example/controller/CollegeController.java
@@ -0,0 +1,54 @@
+package com.example.template.domain.example.controller;
+
+import com.example.template.domain.example.entity.College;
+import com.example.template.domain.example.entity.Coordinate;
+import com.example.template.domain.example.service.CollegeService;
+import lombok.*;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class CollegeController {
+    private final CollegeService collegeService;
+    @PostMapping("/search")
+    CollegeSearchDTO searchCollegesByDistance(@RequestBody CollegeSearchParamDTO param) {
+        if (param.getAcr()==null||param.getDwn()==null) throw new NullPointerException();
+        Coordinate coordinate = Coordinate.builder()
+                .acr(param.getAcr())
+                .dwn(param.dwn).build();
+        if(param.getPage()==null)
+            return new CollegeSearchDTO(collegeService.searchCollegesByDistance(coordinate));
+        else return new CollegeSearchDTO(collegeService.searchCollegesByDistance(coordinate, param.getPage()));
+
+    }
+
+    @PostMapping("/search/length")
+    long numOfColleges() {
+        return collegeService.numOfCollege();
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class CollegeSearchParamDTO{
+        private Double acr;
+        private Double dwn;
+        private Integer page; //Nullable
+    }
+
+    @Data
+    private class CollegeSearchDTO{
+        private List<College> result;
+        private int pageCount;
+
+        CollegeSearchDTO(Page<College> resData) {
+            result = resData.toList();
+            pageCount = resData.getTotalPages();
+        }
+    }
+}
---
+++ src/main/java/com/example/template/domain/example/repository/CollegeRepository.java
@@ -0,0 +1,16 @@
+package com.example.template.domain.example.repository;
+
+import com.example.template.domain.example.entity.College;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CollegeRepository extends JpaRepository<College, Long> {
+    @Query("select c from College c order by"+
+            " (coalesce(c.coordinate.acr,0.0) - :acr)*(coalesce(c.coordinate.acr,0.0) - :acr) +"+
+            " (coalesce(c.coordinate.dwn,0.0) - :dwn)*(coalesce(c.coordinate.dwn,0.0) - :dwn)")
+    Page<College> searchCollegesByDistance(Pageable pageable, double acr, double dwn);
+}
