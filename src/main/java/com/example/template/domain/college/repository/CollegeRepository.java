6173704043124115133279e0c18380603f75f294
YamYamee <cartcnx@naver.com>
2025-02-09 03:59:42 +0900

    원상 복구

--- src/main/java/com/example/template/domain/college/repository/CollegeRepository.java
+++ src/main/java/com/example/template/domain/college/repository/CollegeRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -25,11 +24,9 @@ public interface CollegeRepository extends JpaRepository<College, Long> {
             "THEN true ELSE false END) from College c ";
 
     @Query(getFavorites + "where c.name like :collegeName")
-    Page<CollegeResponseDto> findAllByNameWithFavorites(Pageable pageable, @Param("collegeName")String collegeName, @Param("member") Member member);
-
+    Page<CollegeResponseDto> findAllByNameWithFavorites(Pageable pageable, String collegeName, Member member);
     @Query(getFavorites + "where c.program like :programName")
-    Page<CollegeResponseDto> findAllByProgramWithFavorites(Pageable pageable, @Param("collegeName")String collegeName, @Param("member") Member member);
-
+    Page<CollegeResponseDto> findAllByProgramWithFavorites(Pageable pageable, String programName, Member member);
     @Query(getFavorites + "order by"+
             " (coalesce(c.coordinate.acr,0.0) - :acr)*(coalesce(c.coordinate.acr,0.0) - :acr) +"+
             " (coalesce(c.coordinate.dwn,0.0) - :dwn)*(coalesce(c.coordinate.dwn,0.0) - :dwn)")
