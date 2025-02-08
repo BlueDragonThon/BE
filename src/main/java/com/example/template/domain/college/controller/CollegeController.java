32bd3217303607632fea43bf4e3b64f623a20d3e
YamYamee <cartcnx@naver.com>
2025-02-08 19:04:35 +0900

    실수

--- src/main/java/com/example/template/domain/college/controller/CollegeController.java
+++ src/main/java/com/example/template/domain/college/controller/CollegeController.java
@@ -12,11 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "캐스트 API", description = "캐스트 관련 API입니다")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/example")
-public class ExampleController {
+@RequestMapping("/api/college")
+public class CollegeController {
 
-    @GetMapping("/keyword")
-    @Operation(summary = "예시 API 입니다")
+    @GetMapping("/recommend")
+    @Operation(summary = "사용자 정보를 바탕으로 추천해주는 api")
     public ApiResponse<String> createCastByKeyword() {
         return ApiResponse.of(SuccessCode._OK, "성공입니다");
     }
---
+++ src/main/java/com/example/template/domain/member/dto/MemberRequestDto.java
@@ -0,0 +1,7 @@
+package com.example.template.domain.member.dto;
+
+public class MemberRequestDto {
+    String name;
+    int age;
+    double
+}
